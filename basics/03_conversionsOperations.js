8e19a5ca4b9112d2320b3a98491f7b65c2d098c7
payalkharb1924 <145035007+payalkharb1924@users.noreply.github.com>
2023-09-14 16:51:32 +0000

    conversions

---
+++ basics/03_conversionsOperations.js
@@ -0,0 +1,17 @@
+let score = "33abc"
+let score2 = null
+let score3 = undefined
+// true => 1 , false => 0
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// let valueInNumber2 = Number(score2)
+// let valueInNumber3 = Number(score3)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// console.log(valueInNumber2)
+// console.log(valueInNumber3)
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
