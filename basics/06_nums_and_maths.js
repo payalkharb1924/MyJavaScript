f53272f166ed5675e454c803d36301ff200c945c
payalkharb1924 <145035007+payalkharb1924@users.noreply.github.com>
2023-09-15 16:32:28 +0000

    math and nums

---
+++ basics/06_nums_and_maths.js
@@ -0,0 +1,53 @@
+const score = 100
+// console.log(score);
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+
+const num = 23.873
+// console.log(num.toPrecision(3));
+const num2 = 123.63462
+// console.log(num2.toPrecision(3));
+
+const num3 = 1000000000
+// console.log(num3.toLocaleString());
+// console.log(num3.toLocaleString('en-In'));
+
+
+// *********************MATHS*********************************
+
+// to know properties of maths search Math fcn in incpect console
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.78));
+// console.log(Math.floor(4.78));
+// console.log(Math.ceil(4.78)); // if value is a little moreb than floor value than it will show value+1 i.e 5 here
+// console.log(Math.min(4,5,6,8));
+
+// console.log(Math.random()); // value b/w 0 to 1
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor((Math.random()*10)) + 1);
+
+const min = 10
+const max = 20
+// FORMULA FOR RANDOM NO. B/W SOME VALUES
+console.log(Math.floor(Math.random() * (max - min +1)) + 10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
